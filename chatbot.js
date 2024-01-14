6030d33f0f8605c37595dc12a10019330fe17a7d
Seannnn55 <sally99h@gmail.com>
2024-01-14 21:22:47 +0900

    api key import

--- chatbot.js
+++ chatbot.js
@@ -1,4 +1,4 @@
-
+import { OPENAI_API_KEY } from "./chatbotapikey.js";
 
 // 채팅 메시지를 표시할 DOM
 const chatMessages = document.querySelector('#chat-messages');
@@ -8,11 +8,7 @@ const userInput = document.querySelector('#user-input input');
 const sendButton = document.querySelector('#user-input button');
 // 발급받은 OpenAI API 키를 변수로 저장
 
-// const apiKey = " api key"
-
-// require('dotenv').config();
-
-const apiKey=" openai api key";
+const apiKey= OPENAI_API_KEY;
 
 // OpenAI API 엔드포인트 주소를 변수로 저장
 const apiEndpoint = 'https://api.openai.com/v1/chat/completions'
