e187b0d0d622701ee51c021ff5fc904269121862
Seannnn55 <sally99h@gmail.com>
2024-01-15 00:39:44 +0900

    svg file uploaded

--- chatbot.js
+++ chatbot.js
@@ -1,4 +1,4 @@
-import { OPENAI_API_KEY } from "./chatbotapikey.js";
+// import { OPENAI_API_KEY } from "./chatbotapikey.js";
 
 // 채팅 메시지를 표시할 DOM
 const chatMessages = document.querySelector('#chat-messages');
@@ -8,7 +8,7 @@ const userInput = document.querySelector('#user-input input');
 const sendButton = document.querySelector('#user-input button');
 // 발급받은 OpenAI API 키를 변수로 저장
 
-const apiKey= OPENAI_API_KEY;
+const apiKey= "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";
 
 // OpenAI API 엔드포인트 주소를 변수로 저장
 const apiEndpoint = 'https://api.openai.com/v1/chat/completions'
